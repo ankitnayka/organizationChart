2b6999229250376beecf292f1b7828654a8ed6c0
ankitnayka <ankitnayka2802@gmail.com>
2025-02-24 09:32:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import OrgChart from './components/OrgChart'
+
+
+ const App = () => {
+    
+  return (
+    
+    <>
+  
+    <OrgChart />
+    </>
+  )
+}
+
+
+export default App
